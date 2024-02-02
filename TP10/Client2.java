dbb5d181aac9124731759bc6b704f215b624168b
Tomr76 <tom.richard@univ-rouen.fr>
2024-02-02 09:40:59 +0000

    MAJ tp10

--- TP10/Client2.java
+++ TP10/Client2.java
@@ -1,14 +1,12 @@
 // Import des bibliothèques nécessaires
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
-public class Client1 {
+public class Client2 {
 
     public static void main(String[] args) {
         // Q1.2 - Vérifier le nombre d'arguments
@@ -27,18 +25,10 @@ public class Client1 {
             System.out.println("Executing request " + request.getRequestLine());
             CloseableHttpResponse resp = client.execute(request);
 
-            // Q1.5 - Récupération de la page HTML
-            BufferedReader rd = new BufferedReader(new InputStreamReader(resp.getEntity().getContent()));
-            StringBuffer result = new StringBuffer();
-            String line = " ";
-            while ((line = rd.readLine()) != null) {
-                result.append(line);
-                result.append("\n"); // pour avoir le saut de ligne
-            }
+            // Q2.1 - Utilisation d'InputStreamReader directement
+            InputStreamReader isr = new InputStreamReader(resp.getEntity().getContent());
 
-            // Affichage de la page HTML
-            String page = result.toString();
-            System.out.println(page);
+            // Lire le flux JSON directement avec un parseur JSON (non inclus dans ce code)
 
             resp.close();
         } catch (IOException e) {
